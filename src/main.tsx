0270275bee41c02000f80521b9b76179f2348e80
Kalai <kalaiblazer14@gmail.com>
2026-06-30 21:45:30 +0530

    Fixed

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/react_tutorial_app'>
       <App />
     </BrowserRouter>
   </StrictMode>
